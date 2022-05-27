efcdf326f910d3aa46900c02e3e35df4e98bf56d
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-05-27 11:41:04 +0200

    Inizializzo progetto

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>F</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterComp'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+    <h1>H</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
